95e6a8f7644a81f2590f80ed29d6599ff896c27a
김다인 <dainkim@woowahan.com>
2020-07-01 22:58:50 +0900

    set position of camera and spaceship, add script to move forward in direction of camera

---
+++ Assets/SpaceShipMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipMovement : MonoBehaviour
+{
+    public GameObject Head;
+    public GameObject SpaceShip;
+    public GameObject Cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Head.transform.Translate(Cam.transform.forward * Time.deltaTime * 5f);
+    }
+}
