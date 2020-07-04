8e37fedd671aec091fcc9fd1a6a3bc77ce6c9e69
김다인 <dainkim@woowahan.com>
2020-07-04 17:42:42 +0900

    add Asteroid destory script, project setting to build

---
+++ Assets/Asteroid.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        explosion();
+    }
+    void explosion()
+    {
+        Destroy(gameObject);
+    }
+}
