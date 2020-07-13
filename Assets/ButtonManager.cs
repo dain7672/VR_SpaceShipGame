f490ae27e58e9cd026acedb3e6c766d003c11fd7
김다인 <dainkim@woowahan.com>
2020-07-13 22:57:04 +0900

    scene 처리 구현

---
+++ Assets/ButtonManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void goToGame() // button을 위한 함수는 항상 public이어야 한다. (외부에서 호출하기 위해서 그런듯)
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void goToSelect()
+    {
+        SceneManager.LoadScene("SelectScene");
+    }
+}
---
+++ Assets/SelectShip.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectShip : MonoBehaviour
+{
+    public void shipBlue()
+    {
+        PlayerPrefs.SetInt("ShipType", 1);
+        SceneManager.LoadScene("GameScene");
+    }
+    
+    public void shipRed()
+    {
+        PlayerPrefs.SetInt("ShipType", 2);
+        SceneManager.LoadScene("GameScene");
+    }
+}
