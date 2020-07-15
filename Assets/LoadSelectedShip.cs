6e4de14f4a504ed674b40346f5006ecb6bd6532b
김다인 <dainkim@woowahan.com>
2020-07-15 21:32:35 +0900

    load selected ship / build settings complete

---
+++ Assets/LoadSelectedShip.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSelectedShip : MonoBehaviour
+{
+    public GameObject ship01;
+    public GameObject ship02;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PlayerPrefs.GetInt("ShipType") == 2)
+        {
+            ship02.SetActive(true);
+        }
+        else
+        {
+            ship01.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
